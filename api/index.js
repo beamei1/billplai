d63d61d0189e1baac3a1577144f91eb5b0bd84f6
beamei1 <72058892+beamei1@users.noreply.github.com>
2026-06-14 18:21:03 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,17 @@
 export default async function handler(req, res) {
   try {
-    const GAS_URL =
-      "https://script.google.com/macros/s/AKfycbyhZ3kmNwltzCzufoSWllj4sCtAcBBoddr74H_6oMZYIBptnGRhYJsaAZBitPS_6lmzPQ/exec";
-
-    const response = await fetch(GAS_URL, {
-      method: req.method,
-    });
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbyhZ3kmNwltzCzufoSWllj4sCtAcBBoddr74H_6oMZYIBptnGRhYJsaAZBitPS_6lmzPQ/exec"
+    );
 
     const data = await response.text();
 
+    res.setHeader("Content-Type", "application/json");
     res.status(200).send(data);
+
   } catch (err) {
     res.status(500).json({
-      error: err.toString(),
+      error: err.toString()
     });
   }
 }
